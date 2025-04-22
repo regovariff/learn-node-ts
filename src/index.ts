c42b6656cff8aa4662f782d24c2c2243e63bc69f
Ariff <ariff@regovtech.com>
2025-04-22 17:17:38 +0800

    Add cors setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = ['http://localhost:5000', 'http://localhost:5173'];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true,
+    }),
+);
 
 // Middleware
 app.use(express.json());
